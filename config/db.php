1538e42543817be187e9fa8dba4c38cc59972ade
Andi Irham <Andi.irhamm@gmail.com>
2021-06-25 18:38:06 +0700

    update db setting

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
     'username' => 'root',
-    'password' => 'b4r4mij',
+    'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
